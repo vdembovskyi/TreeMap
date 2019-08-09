225c15456505e18dc0f34c3c1ae21800978e027c
v.dembovskyi <v.dembovskyi@levi9.com>
2019-08-09 11:23:13 +0300

    Homework (Section 12)

---
+++ src/com/timbuchalka/Main.java
@@ -0,0 +1,75 @@
+package com.timbuchalka;
+
+public class Main {
+    private static StockList stockList = new StockList();
+
+    public static void main(String[] args) {
+	    StockItem temp = new StockItem("bread", 0.86, 100);
+        stockList.addStock(temp);
+
+        temp = new StockItem("cake", 1.10, 7);
+        stockList.addStock(temp);
+
+        temp = new StockItem("car", 12.50, 2);
+        stockList.addStock(temp);
+
+        temp = new StockItem("chair", 62.0, 10);
+        stockList.addStock(temp);
+
+        temp = new StockItem("cup", 0.50, 200);
+        stockList.addStock(temp);
+
+        temp = new StockItem("door", 72.95, 4);
+        stockList.addStock(temp);
+
+        temp = new StockItem("juice", 2.50, 36);
+        stockList.addStock(temp);
+
+        temp = new StockItem("phone", 96.99, 35);
+        stockList.addStock(temp);
+
+        temp = new StockItem("towel", 2.40, 80);
+        stockList.addStock(temp);
+
+        temp = new StockItem("vase", 8.76, 40);
+        stockList.addStock(temp);
+
+        System.out.println(stockList);
+        Basket timsBasket = new Basket("Tim");
+        System.out.println(timsBasket);
+        System.out.println("++++++++");
+        timsBasket.addToBasket(stockList.get("cup"), 2);
+        System.out.println(stockList);
+        System.out.println(timsBasket);
+        sellItem(timsBasket, "cup", 5);
+        System.out.println(stockList);
+        System.out.println(timsBasket);
+
+    }
+
+    public static int sellItem(Basket basket, String item, int quantity) {
+        //retrieve the item from stock list
+        StockItem stockItem = stockList.get(item);
+        if(stockItem == null) {
+            System.out.println("We don't sell " + item);
+            return 0;
+        }
+        if(stockList.reserveStock(item, quantity) != 0) {
+            return basket.addToBasket(stockItem, quantity);
+        }
+        return 0;
+    }
+
+    public static int removeItem(Basket basket, String item, int quantity) {
+        //retrieve the item from stock list
+        StockItem stockItem = stockList.get(item);
+        if(stockItem == null) {
+            System.out.println("We don't sell " + item);
+            return 0;
+        }
+        if(basket.removeFromBasket(stockItem, quantity) == quantity) {
+            return stockList.unreserveStock(item, quantity);
+        }
+        return 0;
+    }
+}
